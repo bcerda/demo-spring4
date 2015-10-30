d5ebcdb62fd92919c4f4104e6c8d3acdd53abb21
bayardocerda <bayardocerda@192.168.1.102>
2015-10-30 14:27:30 +0100

    initial version

---
+++ demo-spring4/src/main/java/com/example/demo/DemoSpring4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring4Application {
+
+	// TODO
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpring4Application.class, args);
+    }
+}
